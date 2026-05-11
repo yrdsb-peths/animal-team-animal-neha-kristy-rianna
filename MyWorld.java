97737b4c0b82ecc430a2f80452d111c4d84da2d6
riannadev <riannaknogl@gmail.com>
2026-05-11 12:00:06 -0400

    label

--- MyWorld.java
+++ MyWorld.java
@@ -5,25 +5,44 @@ import greenfoot.*;
  * @author Rianna 
  * @version May 2026
  */
-public class MyWorld extends World {
+public class MyWorld extends World 
+{
+    private int score = 0;
+    private Label scoreLabel;
+
     public MyWorld() 
     {
         super(600, 400, 1);
         
+        // create the elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
+        
+        // create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
         
         createBurger();
     }
+
+    /**
+     * increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
     
     /**
-     * Create a new apple at random location at top of the screen
+     * Create a new burger at random location at top of the screen
      */
     public void createBurger()
     {
         Burger burger = new Burger();
         int x = Greenfoot.getRandomNumber(600); 
         int y = 0;
+        
         addObject(burger, x, y);
     }
 }
